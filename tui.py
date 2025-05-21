836c88796c6a862461ccf93a4784d4a84a4daec9
stencio <stefano.monal.98@gmail.com>
2025-05-21 09:25:56 +0200

    OK

--- tui.py
+++ tui.py
@@ -3,6 +3,8 @@ import subprocess
 import sys
 import os
 from questionary import Choice
+from questionary import Style
+
 import packages
 
 from packages import programs
@@ -11,9 +13,20 @@ choices = [
     Choice(prog, value=prog) for prog in programs.keys()
 ]
 
+
+orange_heat = Style([
+    ("qmark", "fg:#FFA500 bold"),
+    ("question", "fg:#FF8C00 bold"),
+    ("selected", "fg:#FFD700"),
+    ("pointer", "fg:#FF4500 bold"),
+    ("instruction", "fg:#FF6347"),
+])
+
+
 selected = questionary.checkbox(
     "Make sure that snapd or flatpak are installed (first 3 rows) before select a snap or flatpak",
-    choices=choices
+    choices=choices,
+    style=orange_heat
 ).ask()
 
 if not selected:
@@ -23,5 +36,5 @@ else:
         print(f"\n\t🔧 ⚙️  🛠️  {prog}")
         subprocess.run(programs[prog], shell=True)
         
-    print("\n✅  \033[5mMaybe you have to Restart Session.\033[0m\n\n")
+    print("\n✅  \033[1mMaybe you have to Restart Session.\033[0m\n\n")
 
