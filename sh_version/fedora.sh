3f3ad985f7e884c2e3738fd8201908a635011128
stencio <stefano.monal.98@gmail.com>
2025-05-29 12:21:50 +0200

    OK

--- sh_version/fedora.sh
+++ sh_version/fedora.sh
@@ -5,56 +5,48 @@ bold='\e[1m'
 err='\a\033[7m ERROR:'
 reset='\033[0m'
 
-echo -e "${bold}Welcome to Fedora Script Installation\nFirst of all we need to update dnf packages...${reset}"
+echo -e "${bold} Welcome to Fedora Script Installation ${reset}"
+echo -n -e "${bold} Press [ENTER] to continue... ${reset}"
+read
 sleep 2
 
-# First, clean the dnf cache
-dnf clean all
-if [ "$?" -ne 0 ]; then
-	echo -e "${err} dnf clean all ${reset}"
-	exit 1
-fi
+function exec_command(){
+	sudo "$@"
+	#sudo "$*" --> see difference between $@ e $*
+	if [ "$?" -ne 0 ]; then
+		echo -e "${err} $@ ${reset}"
+		echo -e "\n${bold} Do you want to end installation now? [y/n] ${reset}"
+		read -n1 -s INPUT
+		if [[ "$INPUT" = "y" || "$INPUT" = "Y" ]]; then
+			exit 0;
+		fi
+		echo -e "${bold} Skiping command: $@ ${reset}"
+	fi
+}
 
-# Then, update the system
-dnf update -y
-if [ "$?" -ne 0 ]; then
-	echo -e "${err} dnf update ${reset}"
-	exit 1
-fi
+#update dnf packages
+echo -e "${bold} Updating dnf packages ${reset}"
+exec_command dnf clean all
+exec_command dnf update -y
+exec_command dnf upgrade --refresh -y
+exec_command dnf autoremove -y
 
-# Upgrade with refresh
-dnf upgrade --refresh -y
-if [ "$?" -ne 0 ]; then
-	echo -e "${err} dnf upgrade --refresh ${reset}"
-	exit 1
-fi
-
-# Remove unnecessary dependencies
-dnf autoremove -y
-if [ "$?" -ne 0 ]; then
-	echo -e "${err} dnf autoremove ${reset}"
-	exit 1
-fi
 echo -e "${bold} dnf: System updated and cleaned successfully. ${reset}"
 
-echo -e "\nDo you want to continue installation? y/n"
-read -n1 -s INPUT
-if [[ "$INPUT" = "y" || "$INPUT" = "Y" ]]; then
-	echo -e "Do you want to install dnf packages? y/n"
-	read -n1 -s INPUT
-	if [[ "$INPUT" = "y" || "$INPUT" = "Y" ]]; then
-		#open script dnf
-		sh fedora_dnf.sh
-	fi
-	
-	echo -e "\nDo you want to install flatpak packages? y/n"
-	read -n1 -s INPUT
-	if [[ "$INPUT" = "y" || "$INPUT" = "Y" ]]; then
-		#open script flatpak
-		sh fedora_flatpak.sh
-	fi	
-fi
+echo -e "${bold} Prepare Flatpak ${reset}"
+exec_command dnf install -y flatpak
+exec_command flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-echo -e "${bold}Have a nice Linux experience!!"
-exit 0
+echo -e "${bold} Installation Flatpak Packages ${reset}"
+exec_command flatpak install -y flathub org.kde.kate
+exec_command flatpak install -y flathub com.discordapp.Discord
+exec_command flatpak install -y flathub org.chromium.Chromium
+exec_command flatpak install -y flathub com.jetbrains.IntelliJ-IDEA-Community
+exec_command flatpak install -y flathub com.google.AndroidStudio
+exec_command flatpak install -y flathub com.visualstudio.code
+exec_command flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
+exec_command flatpak install -y flathub com.spotify.Client
+exec_command flatpak install -y flathub com.valvesoftware.Steam
+exec_command flatpak install -y flathub org.telegram.desktop
+exec_command flatpak install -y flathub com.play0ad.zeroad
 
