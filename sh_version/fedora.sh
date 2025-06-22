6acaea33a4fc8c777799a9321a67e413ca3efdc5
Stencio98 <121492493+Stencio98@users.noreply.github.com>
2025-06-22 13:55:29 +0200

    Update fedora.sh

--- sh_version/fedora.sh
+++ sh_version/fedora.sh
@@ -39,19 +39,19 @@ exec_command dnf install -y flatpak
 exec_command flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 echo -e "${bold} Update Flatpak packages ${reset}"
-exec_command flatpak update -y
+exec_command flatpak update
 
-echo -e "\n${bold} dnf & flatpak: updated successfully\nDo you want to continue and install additional programs? [y/n] ${reset}"
+echo -e "\n${bold} dnf & flatpak are updated\nDo you want to continue and install additional programs? [y/n] ${reset}"
 read -n1 -s INPUT
 
 if [[ "$INPUT" = "y" || "$INPUT" = "Y" ]]; then
 	echo -e "${bold} All Ready.. Installation Flatpak Packages and dnf packages.. ${reset}"
-	#exec_command flatpak install -y flathub org.kde.kate
+	exec_command flatpak install -y flathub org.kde.kate
 	exec_command flatpak install -y flathub com.discordapp.Discord
 	exec_command flatpak install -y flathub org.chromium.Chromium
 	exec_command flatpak install -y flathub com.jetbrains.IntelliJ-IDEA-Community
 	exec_command flatpak install -y flathub com.google.AndroidStudio
-	#exec_command flatpak install -y flathub com.visualstudio.code
+	exec_command flatpak install -y flathub com.visualstudio.code
 	exec_command flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
 	exec_command flatpak install -y flathub com.spotify.Client
 	exec_command flatpak install -y flathub com.valvesoftware.Steam
