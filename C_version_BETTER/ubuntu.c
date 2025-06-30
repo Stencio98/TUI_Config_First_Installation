8ce908c04dc36d002e4fe80f53344abec7a088d6
stencio <stefano.monal.98@gmail.com>
2025-07-01 00:22:37 +0200

    OK

--- C_version_BETTER/ubuntu.c
+++ C_version_BETTER/ubuntu.c
@@ -1,31 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define INSTALLATION
 
-int esegui_comando(const char *comando) {
-    int status = system(comando);
+int exec_command(const char *command) {
+    int status = system(command);
     if (status == -1) {
-        perror("Errore nell'esecuzione del comando");
+        perror("\033[1merror exec_command:\033[0m\n");
+        fprintf(stdout, "%s", command);
         return 1;
     }
     return 0;
 }
 
 int main() {
-    printf("Aggiornamento della lista dei pacchetti...\n");
-    if (esegui_comando("sudo apt-get update") != 0) return 1;
+    printf("\033[1mAUpdating package list...\033[0m\n");
+    if (exec_command("sudo apt-get update") != 0) return 1;
 
-    printf("Aggiornamento dei pacchetti installati...\n");
-    if (esegui_comando("sudo apt-get upgrade -y") != 0) return 1;
+    printf("\033[1mUpdating installed packages...\033[0m\n");
+    if (exec_command("sudo apt-get upgrade -y") != 0) return 1;
 
-    printf("Rimozione dei pacchetti non necessari...\n");
-    if (esegui_comando("sudo apt-get autoremove -y") != 0) return 1;
+    printf("\033[1mRemoving unused packages...\033[0m\n");
+    if (exec_command("sudo apt-get autoremove -y") != 0) return 1;
 
-    printf("Pulizia dei pacchetti obsoleti...\n");
-    if (esegui_comando("sudo apt-get autoclean -y") != 0) return 1;
+    printf("\033[1mCleaning old packages...\033[0m\n");
+    if (exec_command("sudo apt-get autoclean -y") != 0) return 1;
 
-    printf("Per fare l'avanzamento di versione occorre eseguire:\nsudo do-release-upgrade\n");
+    printf("\033[1mto upgrade version of operative system (for example from ubuntu 22 to ubuntu 24):\nsudo do-release-upgrade\033[0m\n");
 
-    printf("Aggiornamento completato.\n");
+    printf("\033[1mUpdating snap packages...\033[0m\n");
+
+    printf("\033[1mUpdate completed.\033[0m\n");
+    
+#ifdef INSTALLATION
+    const char* list[] = {
+    	"discord (snapd)",
+    	"telegram (snapd)",
+    	"steam (snapd)",
+    	"spotify (snapd)",
+    	"virtualbox (apt)",
+    	
+    	
+    }
+#else
+#endif
     return 0;
+
 }
 
